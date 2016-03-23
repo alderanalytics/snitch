df18b8a9e9ff3336ac7bca29151f7963d533dd36
Alfred Rossi <alfredr@gmail.com>
2016-03-23 15:07:37 -0400

    add sentry reporter

---
+++ sentry/reporter.go
@@ -0,0 +1,27 @@
+package sentry
+
+import (
+	"fmt"
+
+	"github.com/alderanalytics/snitch"
+	"github.com/getsentry/raven-go"
+)
+
+// SentryReporter Reporter implements ErrorReporter via Sentry.
+type SentryReporter struct {
+	client *raven.Client
+}
+
+func NewSentryReporter(client *raven.Client) *SentryReporter {
+	return &SentryReporter{client: client}
+}
+
+// Notify notifies errors via the SentryReporter.
+func (sr SentryReporter) Notify(ectx *snitch.ErrorContext) {
+	tags := make(map[string]string)
+	for k, v := range ectx.Details {
+		tags[k] = fmt.Sprint(v)
+	}
+
+	sr.client.CaptureMessage(ectx.Error, tags)
+}
